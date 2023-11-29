4db8606ce161db4bf9e05dbd3893d188a91a2636
junyeong-0825 <scv9761@gmail.com>
2023-11-29 11:01:50 +0900

    [Add] CameraMove
    카메라가 캐릭터를 따라 움직임

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject target; //카메라가 따라갈 대상
+    public float moveSpeed; //카메라가 따라갈 속도
+    private Vector3 targetPosition; //대상의 현재 위치
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.gameObject != null)
+        {
+            targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+}
